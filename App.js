ad5c1b9a9f9c3549fbd0e7318734c06f7bbea27a
FelipeBattistotti <felipe.battistotti@gmail.com>
2022-07-10 20:01:58 -0300

    Implementação do componente CalendarStrip

--- App.js
+++ App.js
@@ -1,20 +1,99 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import {
+  StyleSheet,
+  View
+} from 'react-native'
+import CalendarStrip from 'react-native-calendar-strip'
+import 'moment/locale/br'
+
+const locale = {
+  name: 'br',
+  config: {
+    months: 'Janeiro_Fevereiro_Março_Abril_Maio_Junho_Julho_Agosto_Setembro_Outubro_Novembro_Dezembro'.split(
+      '_'
+    ),
+    monthsShort: 'Jan_Fev_Mar_Abr_Mai_Jun_Jul_Ago_Set_Out_Nov_Dez'.split(
+      '_'
+    ),
+    weekdays: 'Dimanche_Lundi_Mardi_Mercredi_Jeudi_Vendredi_Samedi'.split('_'),
+    weekdaysShort: 'Dim_Lun_Mar_Mer_Jeu_Ven_Sam'.split('_'),
+    weekdaysMin: 'Di_Lu_Ma_Me_Je_Ve_Sa'.split('_'),
+    longDateFormat: {
+      LT: 'HH:mm',
+      LTS: 'HH:mm:ss',
+      L: 'DD/MM/YYYY',
+      LL: 'D MMMM YYYY',
+      LLL: 'D MMMM YYYY LT',
+      LLLL: 'dddd D MMMM YYYY LT'
+    },
+    calendar: {
+      sameDay: "[Aujourd'hui à] LT",
+      nextDay: '[Demain à] LT',
+      nextWeek: 'dddd [à] LT',
+      lastDay: '[Hier à] LT',
+      lastWeek: 'dddd [dernier à] LT',
+      sameElse: 'L'
+    },
+    relativeTime: {
+      future: 'dans %s',
+      past: 'il y a %s',
+      s: 'quelques secondes',
+      m: 'une minute',
+      mm: '%d minutes',
+      h: 'une heure',
+      hh: '%d heures',
+      d: 'un jour',
+      dd: '%d jours',
+      M: 'un mois',
+      MM: '%d mois',
+      y: 'une année',
+      yy: '%d années'
+    },
+    ordinalParse: /\d{1,2}(er|ème)/,
+    ordinal: function(number) {
+      return number + (number === 1 ? 'er' : 'ème');
+    },
+    meridiemParse: /PD|MD/,
+    isPM: function(input) {
+      return input.charAt(0) === 'M';
+    },
+    // in case the meridiem units are not separated around 12, then implement
+    // this function (look at locale/id.js for an example)
+    // meridiemHour : function (hour, meridiem) {
+    //     return /* 0-23 hour, given meridiem token and hour 1-12 */
+    // },
+    meridiem: function(hours, minutes, isLower) {
+      return hours < 12 ? 'PD' : 'MD';
+    },
+    week: {
+      dow: 1, // Monday is the first day of the week.
+      doy: 4 // The week that contains Jan 4th is the first week of the year.
+    }
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
-  );
+      <View style={styles.container0}>
+        <CalendarStrip
+          scrollable
+          style={{height:200, paddingTop: 20, paddingBottom: 10}}
+          calendarColor={'#3343CE'}
+          calendarHeaderStyle={{color: 'white'}}
+          dateNumberStyle={{color: 'white'}}
+          dateNameStyle={{color: 'white'}}
+          iconContainer={{flex: 0.1}}
+          locale={locale}
+        />
+      </View>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container0: {
+    flex: 1
+  }
+})
